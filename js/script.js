2bd6f9feeecd976b56375aba1de2041f0b96880c
Brian Neaves <brian.g.neaves@gmail.com>
2015-10-08 14:54:38 -0400

    Ajax request works. Input field does not affect the request for Foursquare yet. Need to find out how to access the responseJSON object in the JSON. It seems to have  what I need to show on the map.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 function AppViewModel(){
+	var clientID = "DXJOXHELH1K44MFQUGUWKS2LDYW1FCIFV3YKXBVJSIKNTAZN";
+	var clientSecret = "OFHCOKLL1KHDTB2F4YHNXV5RSQOWICRMQXLOTOXJTNRS2BSQ";
+
 	this.placequery = ko.observable("Where?");
 	this.searchFSquare = function(){
-		console.log("I ran!");
+		var URL = "https://api.foursquare.com/v2/venues/explore?offset=0&limit=50&section=coffee&query=Starbucks&ll=38.0,-78.5&radius=40233.60&client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20121215";
+		var info = $.ajax({
+				type: 'GET',
+				url: URL,
+				dataType: 'json',
+		});
+		
+		console.log(info.readyState);
+		console.log(info.progress);
+		console.log(info);
 	}
 }
 
