40966211733a0efbe860b25ce190dcba3cabc9c1
Brian Neaves <brian.g.neaves@gmail.com>
2015-10-11 17:36:56 -0400

    API call works now. I get the right type of info when I use the search bar.

--- js/script.js
+++ js/script.js
@@ -2,18 +2,31 @@ function AppViewModel(){
 	var clientID = "DXJOXHELH1K44MFQUGUWKS2LDYW1FCIFV3YKXBVJSIKNTAZN";
 	var clientSecret = "OFHCOKLL1KHDTB2F4YHNXV5RSQOWICRMQXLOTOXJTNRS2BSQ";
 
-	this.placequery = ko.observable("Where?");
+	this.userSearch = ko.observable("Where?");
 	this.searchFSquare = function(){
-		var URL = "https://api.foursquare.com/v2/venues/explore?offset=0&limit=50&section=coffee&query=Starbucks&ll=38.0,-78.5&radius=40233.60&client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20121215";
+		alert(this.userSearch());
+		var URL = "https://api.foursquare.com/v2/venues/explore?ll=33.755,-84.390&radius=5000&client_id=" + clientID + "&client_secret=" + clientSecret + "&query=" + this.userSearch()+ "&v=20160101";
 		var info = $.ajax({
 				type: 'GET',
 				url: URL,
 				dataType: 'json',
-		});
+			})
+			.fail(function() {
+    			alert( "error" );
+  			})
+  			.done(function(results) {
+    			alert( "success" );
+    			console.log(results);
+  			});
 		
-		console.log(info.readyState);
-		console.log(info.progress);
-		console.log(info);
+		//console.log(info.readyState);
+		//console.log(info.progress);
+		//console.log(URL);
+		//console.log(info);
+		//console.log(info.responseJSON);
+		
+
+		//console.log(this.userSearch);
 	}
 }
 
