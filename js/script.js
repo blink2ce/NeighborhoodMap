f1f112df51a7c814f14a461fd3cf3917d9f192e7
Brian Neaves <brian.g.neaves@gmail.com>
2015-10-12 14:01:20 -0400

    Maps js code now in script dot js. Also, can now read lat/lng from json

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ function AppViewModel(){
 	var clientID = "DXJOXHELH1K44MFQUGUWKS2LDYW1FCIFV3YKXBVJSIKNTAZN";
 	var clientSecret = "OFHCOKLL1KHDTB2F4YHNXV5RSQOWICRMQXLOTOXJTNRS2BSQ";
 
+
+	var map;
+	function initMap() {
+	  map = new google.maps.Map(document.getElementById('map'), {
+	    center: {lat: 33.755, lng: -84.390},
+	    zoom: 11
+	  });	
+	}
+
+	google.maps.event.addDomListener(window, 'load', initMap);
+
 	this.userSearch = ko.observable("Where?");
 	this.searchFSquare = function(){
 		alert(this.userSearch());
@@ -16,20 +27,29 @@ function AppViewModel(){
   			})
   			.done(function(results) {
     			alert( "success" );
+    			//Populate map with markers
+    			//for each venue in the JSON data between 0 and 10, create a 
+    			//marker on the map and link to that venue's lat/long data.
+    			var myLatlng = new google.maps.LatLng(33.755,-84.390);
+    			var marker = new google.maps.Marker({
+    				position: myLatlng,
+    				title:"Hello World!"
+				});
+    			marker.setMap(map);
     			console.log(results);
+    			var responseLength = results.response.groups[0].items.length;
+    			console.log("response length is " + responseLength);
+    			for(var i = 0; i < responseLength; i++){
+    				console.log(results.response.groups[0].items[i].venue.location.lat);
+    				console.log(results.response.groups[0].items[i].venue.location.lng);
+    			}
+    			
+
   			});
 		
-		//console.log(info.readyState);
-		//console.log(info.progress);
-		//console.log(URL);
-		//console.log(info);
-		//console.log(info.responseJSON);
-		
-
-		//console.log(this.userSearch);
 	}
-}
 
+}
 
 //activates knockout.js
 ko.applyBindings(new AppViewModel());
